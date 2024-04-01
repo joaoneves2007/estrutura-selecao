8b758495c2ff046c83fb476834e4b5d22358c648
João Neves <contatojotaico@gmail.com>
2024-04-01 10:20:55 -0400

    finalizada questao8

--- questao8.js
+++ questao8.js
@@ -4,9 +4,18 @@ determinar se uma idade corresponde a bebê, criança, adolescente, adulto ou
 idoso em JavaScript?
 */
 
-let idade = 25;
+let idade = 30;
 
 if (idade >= 0 && idade <= 2) {
-    console.log("É um bebê.");
+    console.log("A idade corresponde a um bebê.");
 } else if (idade >= 3 && idade <= 12) {
-    console.log("É uma
+    console.log("A idade corresponde a uma criança.");
+} else if (idade >= 13 && idade <= 19) {
+    console.log("A idade corresponde a um adolescente.");
+} else if (idade >= 20 && idade <= 64) {
+    console.log("A idade corresponde a um adulto.");
+} else if (idade >= 65 && idade <= 120) {
+    console.log("A idade corresponde a um idoso.");
+} else {
+    console.log("A idade inserida não é válida.");
+}let idade = 30;
