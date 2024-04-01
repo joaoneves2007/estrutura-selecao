570fa61be7da560b867883c67345bced69c81285
João Neves <contatojotaico@gmail.com>
2024-04-01 09:56:26 -0400

    finalizada questao2

---
+++ questao2.js
@@ -0,0 +1,12 @@
+/*
+Como você pode usar uma declaração if para verificar se uma 
+string está vazia em JavaScript?
+*/
+
+let minhaString = "";
+
+if (minhaString === "") {
+    console.log("A string está vazia.");
+} else {
+    console.log("A string não está vazia.");
+}
