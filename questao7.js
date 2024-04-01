7c6828b0a1fe6c332eb11a11defc3267a9b5c108
João Neves <contatojotaico@gmail.com>
2024-04-01 09:57:06 -0400

    finalizada questao7

---
+++ questao7.js
@@ -0,0 +1,12 @@
+/*
+Como você pode usar uma declaração if, else if, else para verificar 
+se um número é múltiplo de 2, 3 ou 5 em JavaScript?
+*/
+
+let numero = 10;
+
+if (numero % 2 === 0) {
+    console.log("O número é múltiplo de 2.");
+} else if (numero % 3 === 0) {
+    console.log("O número é múltiplo de 3.");
+} else if (numero % 5 === 0)
