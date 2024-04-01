2ad86812d3a6d17ea95e338707c3a19ce602137d
João Neves <contatojotaico@gmail.com>
2024-04-01 10:21:03 -0400

    finalizada questao9

--- questao9.js
+++ questao9.js
@@ -5,9 +5,17 @@ seus lados em JavaScript?
 */
 
 let lado1 = 5;
-let lado2 = 5;
+let lado2 = 4;
 let lado3 = 5;
 
-if (lado1 === lado2 && lado2 === lado3) {
-    console.log("É um triângulo equilátero.");
-} else if (lado1 === lado2 || lado1 === lado3 ||
+if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0) {
+    console.log("Os lados do triângulo devem ter valores positivos.");
+} else if (lado1 + lado2 <= lado3 || lado1 + lado3 <= lado2 || lado2 + lado3 <= lado1) {
+    console.log("Os lados fornecidos não formam um triângulo.");
+} else if (lado1 === lado2 && lado2 === lado3) {
+    console.log("O triângulo é equilátero, todos os lados têm o mesmo comprimento.");
+} else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+    console.log("O triângulo é isósceles, possui dois lados com o mesmo comprimento.");
+} else {
+    console.log("O triângulo é escaleno, todos os lados têm comprimentos diferentes.");
+}
