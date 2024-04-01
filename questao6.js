05cd98368269760c8f66062ff3d0760831376dda
João Neves <contatojotaico@gmail.com>
2024-04-01 09:56:58 -0400

    finalizada questao6

---
+++ questao6.js
@@ -0,0 +1,10 @@
+/*
+Como você pode usar uma declaração if, else if, else para 
+determinar a estação do ano com base no mês em JavaScript?
+*/
+
+let mes = 3; // Por exemplo, março
+
+if (mes >= 3 && mes <= 5) {
+    console.log("Estamos na primavera.");
+} else if (mes >= 6 && mes <= 8) {
